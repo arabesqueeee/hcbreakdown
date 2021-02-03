ca38ff81a568ff840a177726d36e707f62747469
wangyiqiong <wang.yiqiong@accenture.com>
2021-02-03 20:03:52 +0800

    first commit

---
+++ uimodule/webapp/controller/Main.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/tsmc/hcbreakdown/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.tsmc.hcbreakdown.controller.Main", {});
+});
